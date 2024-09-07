52111fb7673fb048c18afecd47c3e2303337142a
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-09-07 22:33:45 +0200

    Add configuration to HelloControllerTest depending on local or server environment

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -3,9 +3,11 @@ package com.example.springboot;
 import static org.hamcrest.Matchers.equalTo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -13,15 +15,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("local")
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @Value("${app.environment}")
+    private String environment;
+
     @Test
     public void getHello() throws Exception {
+      String expectedResponse = "Greetings from Spring Boot running in " + environment + " environment!";
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+                .andExpect(content().string(equalTo(expectedResponse)));
     }
 }

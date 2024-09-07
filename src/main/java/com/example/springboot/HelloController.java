020c6a55a6f3084fcf539e832f5950402c45d237
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-09-07 22:12:14 +0200

    Add environment configuration to HelloController

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,13 +1,17 @@
 package com.example.springboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public  class HelloController {
 
+  @Value("${app.environment}")
+  private String environment;
+
   @GetMapping("/")
   public String index() {
-    return "Greetings from Spring Boot!";
+    return "Greetings from Spring Boot running in " + environment + " environment!";
   }
 }
